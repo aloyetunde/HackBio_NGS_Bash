ca8b215d9014edcdb18fc7adff99b99c1f1fef6b
aloyetunde <107317858+aloyetunde@users.noreply.github.com>
2025-08-28 15:23:29 +0100

    Create stage 0_Project 2.sh

---
+++ stage 0_Project 2.sh
@@ -0,0 +1,46 @@
+1. Activate your base conda environment
+$ conda activate base
+
+2. Create a conda environment named funtools
+$ conda create -n funtools
+
+3. Activate the funtools environment
+$ conda activate funtools
+
+4. Install Figlet using conda or apt-get
+
+Using conda:
+
+$ conda install -c conda-forge figlet
+
+
+Or using apt-get:
+
+$ sudo apt-get install figlet
+
+5. Run figlet with your name
+$ figlet "Alo Yetunde"
+
+6. Install bwa through the bioconda channel
+$ conda install -c bioconda bwa
+
+7. Install blast through the bioconda channel
+$ conda install -c bioconda blast
+
+8. Install samtools through the bioconda channel
+$ conda install -c bioconda samtools
+
+9. Install bedtools through the bioconda channel
+$ conda install -c bioconda bedtools
+
+10. Install spades.py through the bioconda channel
+$ conda install -c bioconda spades
+
+11. Install bcftools through the bioconda channel
+$ conda install -c bioconda bcftools
+
+12. Install fastp through the bioconda channel
+$ conda install -c bioconda fastp
+
+13. Install multiqc through the bioconda channel
+$ conda install -c bioconda multiqc
