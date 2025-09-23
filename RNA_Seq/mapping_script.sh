6ea290d56cdc513ae22bbd69293888c4911f2f29
aloyetunde <107317858+aloyetunde@users.noreply.github.com>
2025-09-23 10:21:53 +0100

    Update mapping_script.sh

--- RNA_Seq/mapping_script.sh
+++ RNA_Seq/mapping_script.sh
@@ -2,10 +2,10 @@
 # script for mapping reads
 
 
-mkdir -p mapped
+mkdir -p mapped_reads
 #! /bin/bash
 for infile in trim/*.fq ; do
 	outfile=$(basename "$infile".fq)
-	STAR --genomeDir genome/genomeIndex --readFilesIn $infile --outFileNamePrefix mapped/$outfile --outSAMtype BAM SortedByCoordinate --outSAMattributes All
+	STAR --genomeDir genome/genomeIndex --readFilesIn $infile --outFileNamePrefix mapped_reads/$outfile --outSAMtype BAM SortedByCoordinate --outSAMattributes All
 done
 
